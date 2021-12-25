f2d91b7b74b41b9af918bb3a36670577561d7db1
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2021-12-25 23:06:30 +0100

    fix urls

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from api.views import feeds
 
 urlpatterns = [
-    path("feeds/<str:age>/<str:attack_type>/<str:format>", feeds),
+    path("feeds/<str:age>/<str:attack_type>/<str:format_>", feeds),
 ]
