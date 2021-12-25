d48416006f0b5627db0c0cf88de1252c19dcdb27
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2021-12-25 16:14:09 +0100

    adjusts

--- greedybear/settings.py
+++ greedybear/settings.py
@@ -127,8 +127,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static_greedybear/"),)
-
 
 INFO_OR_DEBUG_LEVEL = "DEBUG" if DEBUG else "INFO"
 LOGGING = {
