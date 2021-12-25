dc250ee0746c905db0df9ac8f8908d354611af12
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2021-12-25 20:20:01 +0100

    adjusted urls

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from api.views import feeds
 
 urlpatterns = [
-    re_path("^feeds/<age>/<attack_type>/<format>", feeds),
+    re_path("^feeds/<str:age>/<str:attack_type>/<str:format>", feeds),
 ]
