39432648dc7158d47c0623fef5809d2454ac9bf1
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2021-12-25 18:00:45 +0100

    adjust

--- greedybear/urls.py
+++ greedybear/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path, re_path
 urlpatterns = [
     # admin
     path("admin/", admin.site.urls, name="admin"),
-    re_path("^gui/", include("gui.urls")),
+    # re_path("^gui/", include("gui.urls")),
     re_path("^api/", include("api.urls")),
 ]
