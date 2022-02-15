3dd107df62f8e48b4528450c380a6e0a15ab12e5
uzaxirr <uzaxirr@gmail.com>
2022-02-15 21:50:55 +0530

    Format

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+
 from django.test import TestCase
+
 from greedybear.models import IOC
+
 
 class EnrichmentViewTestCase(TestCase):
     @classmethod
@@ -48,9 +51,13 @@ class EnrichmentViewTestCase(TestCase):
             response.json()["ioc"]["last_seen"],
             self.ioc.last_seen.isoformat(sep="T", timespec="microseconds"),
         )
-        self.assertEqual(response.json()["ioc"]["number_of_days_seen"], self.ioc.number_of_days_seen)
+        self.assertEqual(
+            response.json()["ioc"]["number_of_days_seen"], self.ioc.number_of_days_seen
+        )
         self.assertEqual(response.json()["ioc"]["times_seen"], self.ioc.times_seen)
         self.assertEqual(response.json()["ioc"]["log4j"], self.ioc.log4j)
         self.assertEqual(response.json()["ioc"]["cowrie"], self.ioc.cowrie)
         self.assertEqual(response.json()["ioc"]["scanner"], self.ioc.scanner)
-        self.assertEqual(response.json()["ioc"]["payload_request"], self.ioc.payload_request)
+        self.assertEqual(
+            response.json()["ioc"]["payload_request"], self.ioc.payload_request
+        )
