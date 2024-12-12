3fbbcc001ece2eb1513b5ff9c35ab15e92d2c637
tim <tim.leonhard@posteo.de>
2024-12-12 11:38:36 +0100

    adapt test to new feed type validation method

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from itertools import product
 
-from api.serializers import FeedsResponseSerializer, FeedsSerializer
+from api.serializers import FeedsResponseSerializer, FeedsSerializer, feed_type_validation
 from django.test import TestCase
 from greedybear.consts import PAYLOAD_REQUEST, SCANNER
 from greedybear.models import GeneralHoneypot
@@ -32,19 +32,23 @@ class FeedsSerializersTestCase(TestCase):
         for element in valid_data_choices:
             data_ = {"feed_type": element[0], "attack_type": element[1], "age": element[2], "format": element[3]}
             serializer = FeedsSerializer(data=data_)
-            valid = serializer.is_valid(raise_exception=True)
+            valid = serializer.is_valid(raise_exception=True) and feed_type_validation(data_["feed_type"], frozenset(feed_type_choices))
             self.assertEqual(valid, True)
 
     def test_invalid_fields(self):
+        valid_feed_types = frozenset(["all", "log4j", "cowrie", "adbhoney"])
         data_ = {"feed_type": "invalid_feed_type", "attack_type": "invalid_attack_type", "age": "invalid_age", "format": "invalid_format"}
         serializer = FeedsSerializer(data=data_)
         try:
             serializer.is_valid(raise_exception=True)
         except ValidationError:
-            self.assertIn("feed_type", serializer.errors)
             self.assertIn("attack_type", serializer.errors)
             self.assertIn("age", serializer.errors)
             self.assertIn("format", serializer.errors)
+        try:
+            feed_type_validation(data_["feed_type"], valid_feed_types)
+        except ValidationError as e:
+            self.assertIn("feed_type", str(e))
 
 
 class FeedsResponseSerializersTestCase(TestCase):
@@ -79,10 +83,11 @@ class FeedsResponseSerializersTestCase(TestCase):
                 "feed_type": element[2],
             }
             serializer = FeedsResponseSerializer(data=data_)
-            valid = serializer.is_valid(raise_exception=True)
+            valid = serializer.is_valid(raise_exception=True) and feed_type_validation(data_["feed_type"], frozenset(feed_type_choices))
             self.assertEqual(valid, True)
 
     def test_invalid_fields(self):
+        valid_feed_types = frozenset(["all", "log4j", "cowrie", "adbhoney"])
         data_ = {
             "value": True,
             SCANNER: "invalid_scanner",
@@ -102,4 +107,7 @@ class FeedsResponseSerializersTestCase(TestCase):
             self.assertIn("first_seen", serializer.errors)
             self.assertIn("last_seen", serializer.errors)
             self.assertIn("times_seen", serializer.errors)
-            self.assertIn("feed_type", serializer.errors)
+        try:
+            feed_type_validation(data_["feed_type"], valid_feed_types)
+        except ValidationError as e:
+            self.assertIn("feed_type", str(e))
