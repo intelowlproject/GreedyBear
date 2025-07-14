32d542b0d427e18c65be02174e8a7fc0e91a1224
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2025-07-14 22:18:44 +0200

    fix

--- greedybear/cronjobs/mass_scanners.py
+++ greedybear/cronjobs/mass_scanners.py
@@ -9,20 +9,22 @@ class MassScannersCron(Cronjob):
     def run(self) -> None:
         regex_compiled = re.compile(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s*#\s*(.+)*", re.DOTALL)
         r = requests.get("https://raw.githubusercontent.com/stamparm/maltrail/master/trails/static/mass_scanner.txt", timeout=10)
-        for line in r.text:
-            if not line or line.startswith("#"):
-                continue
-            if match := re.match(regex_compiled, line):
-                ip_address = match.group(1)
-                reason = match.group(2)
-                try:
-                    MassScanners.objects.get(ip_address=ip_address)
-                except MassScanners.DoesNotExist:
-                    self.log.info(f"added new mass scanner {ip_address}")
-                    MassScanners(ip_address=ip_address, reason=reason).save()
-                    self._update_old_ioc(ip_address)
-            else:
-                self.log.warning(f"unexpected line: {line}")
+        for line_bytes in r.iter_lines():
+            if line_bytes:
+                line = line_bytes.decode("utf-8")
+                if not line or line.startswith("#"):
+                    continue
+                if match := re.match(regex_compiled, line):
+                    ip_address = match.group(1)
+                    reason = match.group(2)
+                    try:
+                        MassScanners.objects.get(ip_address=ip_address)
+                    except MassScanners.DoesNotExist:
+                        self.log.info(f"added new mass scanner {ip_address}")
+                        MassScanners(ip_address=ip_address, reason=reason).save()
+                        self._update_old_ioc(ip_address)
+                else:
+                    self.log.warning(f"unexpected line: {line}")
 
     def _update_old_ioc(self, ip_address):
         try:
