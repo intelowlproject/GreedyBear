4e42262994c849e84b75af98d018318461338d73
tim <46972822+regulartim@users.noreply.github.com>
2025-12-01 19:35:06 +0100

    CowrieSession API. Closes #446 (#600)
    * add API to access Cowrie session data
    
    * add test cases
    
    * add exception calls behind except keyword
    
    * change block comment style of test separators

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,5 @@
 from api.views.command_sequence import *
+from api.views.cowrie_session import *
 from api.views.enrichment import *
 from api.views.feeds import *
 from api.views.general_honeypot import *
