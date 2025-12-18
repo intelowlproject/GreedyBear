96815750cc1225088c0e9e07b95cea4f1d3c0a50
tim <tim.leonhard@posteo.de>
2025-12-18 12:51:00 +0100

    remove old files

--- greedybear/cronjobs/honeypots.py
+++
@@ -1,9 +0,0 @@
-# This file is a part of GreedyBear https://github.com/honeynet/GreedyBear
-# See the file 'LICENSE' for copying permission.
-from dataclasses import dataclass
-
-
-@dataclass
-class Honeypot:
-    name: str
-    description: str = ""
