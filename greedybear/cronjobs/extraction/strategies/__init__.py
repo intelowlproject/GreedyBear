5f20cee527686b53617b1302ea8d776221d06214
tim <tim.leonhard@posteo.de>
2025-12-18 12:07:19 +0100

    add extraction strategies

---
+++ greedybear/cronjobs/extraction/strategies/__init__.py
@@ -0,0 +1,4 @@
+from greedybear.cronjobs.extraction.strategies.base import *
+from greedybear.cronjobs.extraction.strategies.cowrie import *
+from greedybear.cronjobs.extraction.strategies.generic import *
+from greedybear.cronjobs.extraction.strategies.log4pot import *
