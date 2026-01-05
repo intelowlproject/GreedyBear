a93618fa368bb428d80d4904ad99df235e6b0746
amisha <amishhhaaaa@gmail.com>
2026-01-05 21:11:26 +0530

    Rewriting tests so they can be decoupled.

--- tests/greedybear/cronjobs/test_monitor_logs.py
+++ tests/greedybear/cronjobs/test_monitor_logs.py
@@ -8,10 +8,27 @@ from greedybear.slack import send_message
 
 
 class MonitorLogsTestCase(TestCase):
-    @patch("greedybear.cronjobs.monitor_logs.Path.stat")
     @patch("greedybear.cronjobs.monitor_logs.Path.exists")
+    @patch("greedybear.cronjobs.monitor_logs.Path.stat")
     @patch("greedybear.cronjobs.monitor_logs.send_message")
-    def test_logs_recent_activity(self, mock_send, mock_exists, mock_stat):
+    def test_run_all_recent_logs(self, mock_send, mock_stat, mock_exists):
+        # Setup mock responses
+        mock_exists.return_value = True
+
+        # Simulate all recent activity
+        recent_time = datetime.now().timestamp()
+        mock_stat.return_value.st_mtime = recent_time
+
+        # Run the cronjob
+        cronjob = MonitorLogs()
+        cronjob.execute()
+
+        self.assertEqual(mock_send.call_count, 4)
+
+    @patch("greedybear.cronjobs.monitor_logs.Path.exists")
+    @patch("greedybear.cronjobs.monitor_logs.Path.stat")
+    @patch("greedybear.cronjobs.monitor_logs.send_message")
+    def test_run_some_recent_logs(self, mock_send, mock_stat, mock_exists):
         # Setup mock responses
         mock_exists.return_value = True
 
@@ -36,3 +53,32 @@ class MonitorLogsTestCase(TestCase):
         cronjob.execute()
 
         mock_send.assert_called_once_with("found errors in log file greedybear_errors.log")
+
+    @patch("greedybear.cronjobs.monitor_logs.Path.exists")
+    @patch("greedybear.cronjobs.monitor_logs.Path.stat")
+    @patch("greedybear.cronjobs.monitor_logs.send_message")
+    def test_run_no_recent_logs(self, mock_send, mock_stat, mock_exists):
+        # Setup mock responses
+        mock_exists.return_value = True
+
+        # Simulate no recent activity
+        mock_stat.return_value.st_mtime = (datetime.now() - timedelta(hours=3)).timestamp()
+
+        # Run the cronjob
+        cronjob = MonitorLogs()
+        cronjob.execute()
+
+        mock_send.assert_not_called()
+
+    @patch("greedybear.cronjobs.monitor_logs.Path.exists")
+    @patch("greedybear.cronjobs.monitor_logs.Path.stat")
+    @patch("greedybear.cronjobs.monitor_logs.send_message")
+    def test_run_no_file(self, mock_send, mock_stat, mock_exists):
+        # Setup mock responses
+        mock_exists.return_value = False
+
+        # Run the cronjob
+        cronjob = MonitorLogs()
+        cronjob.execute()
+
+        mock_send.assert_not_called()
