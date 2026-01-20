d189237a04cabb37d6a6bd0232576021be1f4e8a
opbot_xd <awasthikrishna23052005@gmail.com>
2026-01-21 04:29:36 +0530

    Revert "chore: Trigger CI with temporary debug log"
    This reverts commit a7aa9412451558da8eb1b34ae2c2a4ddfd1d166f.

--- greedybear/apps.py
+++ greedybear/apps.py
@@ -1,14 +1,7 @@
 # This file is a part of GreedyBear https://github.com/honeynet/GreedyBear
 # See the file 'LICENSE' for copying permission.
-import logging
-
 from django.apps import AppConfig
-
-logger = logging.getLogger(__name__)
 
 
 class GreedyBearConfig(AppConfig):
     name = "greedybear"
-
-    def ready(self):
-        logger.debug("GreedyBear app ready")  # TODO: Remove before merging PR #701
