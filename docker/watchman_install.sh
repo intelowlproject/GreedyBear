e38dfb63ceb877b86f484d2f8c6dd4b35dc6fc1c
tim <46972822+regulartim@users.noreply.github.com>
2026-02-11 21:55:51 +0100

    switch backend base image from alpine to debian-slim

--- docker/watchman_install.sh
+++ docker/watchman_install.sh
@@ -3,7 +3,7 @@
 # This script can be disabled during development using REPO_DOWNLOADER_ENABLED=true env variable
 if [ "$WATCHMAN" = "false" ]; then echo "Skipping WATCHMAN installation because we are not in test mode"; exit 0;  fi
 
-apk add gcc linux-headers build-base
+apt-get update && apt-get install -y --no-install-recommends gcc build-essential
 pip3 install --compile -r requirements/django-server-requirements.txt
 
 # install Watchman to enhance performance on the Django development Server
