a7fdab4ccf08ec24a4297777ebac4688a45761dc
rootp1 <arnav.iitr@gmail.com>
2026-02-23 22:20:21 +0530

    added intelowl column

--- frontend/src/constants/environment.js
+++ frontend/src/constants/environment.js
@@ -13,3 +13,7 @@ export const VERSION = isTest
 export const PUBLIC_URL = isTest
   ? (process.env.PUBLIC_URL || "/").replace(/\/$/, "")
   : import.meta.env.BASE_URL.replace(/\/$/, "");
+
+export const INTELOWL_URL = isTest
+  ? (process.env.VITE_INTELOWL_URL || "").replace(/\/$/, "")
+  : (import.meta.env.VITE_INTELOWL_URL || "").replace(/\/$/, "");
