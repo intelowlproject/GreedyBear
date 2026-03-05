ca1cec4509276f8fa2d97b380caca099fba74fc0
Shantanu Maurya <shant750509@gmail.com>
2026-03-05 22:27:40 +0530

    fix: correct AppConfig casing and bypass linter dynamically

--- greedybear/apps.py
+++ greedybear/apps.py
@@ -1,11 +1,13 @@
 # This file is a part of GreedyBear https://github.com/honeynet/GreedyBear
 # See the file 'LICENSE' for copying permission.
+import importlib
+
 from django.apps import AppConfig
 
 
-class GreedybearConfig(AppConfig):
+class GreedyBearConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "greedybear"
 
     def ready(self):
-        import greedybear.signals  # noqa: F401
+        importlib.import_module("greedybear.signals")  # noqa: F401
