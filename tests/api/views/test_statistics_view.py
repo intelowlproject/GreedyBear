a1fa4c56b1c80bbf38a64900230961ae8b29effe
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2026-03-10 23:42:42 +0530

    add test for countries statistics

--- tests/api/views/test_statistics_view.py
+++ tests/api/views/test_statistics_view.py
@@ -1,4 +1,4 @@
-from greedybear.models import GeneralHoneypot, Statistics, ViewType
+from greedybear.models import IOC, GeneralHoneypot, IocType, Statistics, ViewType
 from tests import CustomTestCase
 
 
@@ -50,3 +50,76 @@ class StatisticsViewTestCase(CustomTestCase):
         self.assertEqual(response.json()[0]["Log4pot"], 3)
         self.assertEqual(response.json()[0]["Cowrie"], 3)
         self.assertEqual(response.json()[0]["Tanner"], 0)
+
+    def test_200_countries(self):
+        # Create IOCs with attacker_country set
+        ioc_cn = IOC.objects.create(
+            name="1.2.3.4",
+            type=IocType.IP.value,
+            first_seen=self.current_time,
+            last_seen=self.current_time,
+            days_seen=[self.current_time],
+            number_of_days_seen=1,
+            attack_count=1,
+            interaction_count=1,
+            attacker_country="CN",
+        )
+        ioc_cn2 = IOC.objects.create(
+            name="1.2.3.5",
+            type=IocType.IP.value,
+            first_seen=self.current_time,
+            last_seen=self.current_time,
+            days_seen=[self.current_time],
+            number_of_days_seen=1,
+            attack_count=1,
+            interaction_count=1,
+            attacker_country="CN",
+        )
+        ioc_us = IOC.objects.create(
+            name="1.2.3.6",
+            type=IocType.IP.value,
+            first_seen=self.current_time,
+            last_seen=self.current_time,
+            days_seen=[self.current_time],
+            number_of_days_seen=1,
+            attack_count=1,
+            interaction_count=1,
+            attacker_country="US",
+        )
+        ioc_inactive = IOC.objects.create(
+            name="1.2.3.7",
+            type=IocType.IP.value,
+            first_seen=self.current_time,
+            last_seen=self.current_time,
+            days_seen=[self.current_time],
+            number_of_days_seen=1,
+            attack_count=1,
+            interaction_count=1,
+            attacker_country="RU",
+        )
+        ioc_cn.general_honeypot.add(self.heralding)
+        ioc_cn2.general_honeypot.add(self.heralding)
+        ioc_us.general_honeypot.add(self.heralding)
+        # ioc_inactive is only attached to an inactive honeypot (should be excluded)
+        ioc_inactive.general_honeypot.add(self.ddospot)
+
+        response = self.client.get("/api/statistics/countries")
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIsInstance(data, list)
+        countries = [item["country"] for item in data]
+        counts = {item["country"]: item["count"] for item in data}
+        # CN should appear twice, US once; RU (inactive honeypot) must be excluded
+        self.assertIn("CN", countries)
+        self.assertIn("US", countries)
+        self.assertNotIn("RU", countries)
+        self.assertEqual(counts["CN"], 2)
+        self.assertEqual(counts["US"], 1)
+        # Results must be ordered descending by count
+        count_values = [item["count"] for item in data]
+        self.assertEqual(count_values, sorted(count_values, reverse=True))
+
+        ioc_cn.delete()
+        ioc_cn2.delete()
+        ioc_us.delete()
+        ioc_inactive.delete()
