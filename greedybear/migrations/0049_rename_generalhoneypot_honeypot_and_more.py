8b3c5a1da59bee84db0d1e52175440c09cf8ea6d
tim <46972822+regulartim@users.noreply.github.com>
2026-04-02 08:28:05 +0200

    rename migration

--- greedybear/migrations/0049_rename_generalhoneypot_honeypot_and_more.py
+++ greedybear/migrations/0049_rename_generalhoneypot_honeypot_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("greedybear", "0047_credential_sources"),
+        ("greedybear", "0048_add_attacker_country_code"),
     ]
 
     operations = [
