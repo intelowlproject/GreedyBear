3c053e8160551962381870a9347ebe37f8c68135
Vipeen <vipeenk023@gmail.com>
2026-04-09 10:12:21 +0530

    Fix include_similar returning invalid sessions (duration <= 0) and add regression test

--- api/views/cowrie_session.py
+++ api/views/cowrie_session.py
@@ -101,7 +101,9 @@ def cowrie_session_view(request):
     if include_similar:
         commands = {s.commands for s in sessions if s.commands}
         clusters = {cmd.cluster for cmd in commands if cmd.cluster is not None}
-        related_sessions = CowrieSession.objects.filter(commands__cluster__in=clusters).prefetch_related("source", "commands", "credentials")
+        related_sessions = CowrieSession.objects.filter(commands__cluster__in=clusters, duration__gt=0).prefetch_related(
+            "source", "commands", "credentials"
+        )
         sessions = sessions.union(related_sessions)
 
     response_data = {
