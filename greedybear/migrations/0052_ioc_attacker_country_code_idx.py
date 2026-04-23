ae4441b16ee0fd59d7789bfbdb6bd073fe1367a5
Manik <maniksingh3606@gmail.com>
2026-04-23 18:45:30 +0530

    fix migration chain after merging develop

--- greedybear/migrations/0052_ioc_attacker_country_code_idx.py
+++ greedybear/migrations/0052_ioc_attacker_country_code_idx.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("greedybear", "0050_attackeractivitybucket"),
+        ("greedybear", "0051_alter_statistics_source_length"),
     ]
 
     operations = [
