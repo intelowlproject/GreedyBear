f56a38ff4b9970cf9d304b391d771a2605f5a18c
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2026-05-13 12:24:35 +0530

    removing unused imports and compatibility layer

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,23 +1,13 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import importPlugin from "eslint-plugin-import";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended"),
+export default defineConfig([
+    js.configs.recommended,
+    {
 
     languageOptions: {
         globals: {
